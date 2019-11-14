22133eb8fbe4317ef2c76863c5a42e84f7b54be1
skullquake <ockert8080@gmail.com>
2019-11-13 23:41:10 -0800

    .

--- src/MxBabylonJS/widget/MxBabylonJS.js
+++ src/MxBabylonJS/widget/MxBabylonJS.js
@@ -32,6 +32,7 @@ require(
 		'dijit/_TemplatedMixin',
 		'mxui/dom',
 		'dojo/dom',
+		'dojo/dom-attr',
 		'dojo/dom-prop',
 		'dojo/dom-geometry',
 		'dojo/dom-class',
@@ -52,6 +53,7 @@ require(
 		_WidgetBase,
 		_TemplatedMixin,
 		dom,
+		domAttr,
 		dojoDom,
 		dojoProp,
 		dojoGeometry,
@@ -86,70 +88,16 @@ require(
 					this._handles=[];
 				},
 				postCreate:function(){
-					mx.ui.info('3');
-					window.wid=this;
-					this.canvas=dojo.create(
-						"canvas",
-						{
-							'id':'renderCanvas',//this.id+'_canvas',
-							'touch-action':'none',
-							'width':'320px',
-							'height':'320px',
-							'style':'width:100%;'
-						}
-					);
-					this.domNode.appendChild(
-						this.canvas
-					);
-					//this.test0();
+					this.domNode.appendChild(this.canvas);
 				},
-				test0:function(){
-					var canvas = document.getElementById("renderCanvas"); // Get the canvas element 
-					var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-
-					/******* Add the create scene function ******/
-					var createScene = function () {
-
-						// Create the scene space
-						var scene = new BABYLON.Scene(engine);
-
-						// Add a camera to the scene and attach it to the canvas
-						var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), scene);
-						camera.attachControl(canvas, true);
-
-						// Add lights to the scene
-						var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-						var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
-
-						// Add and manipulate meshes in the scene
-						var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
-
-						return scene;
-					};
-					/******* End of the create scene function ******/	
-
-					this.scene = createScene(); //Call the createScene function
-
-					// Register a render loop to repeatedly render the scene
-					engine.runRenderLoop(function () { 
-							this.scene.render();
-					});
-
-					// Watch for browser/canvas resize events
-					window.addEventListener("resize", function () { 
-							engine.resize();
-					});
-
-				},
-				test1:function(urlP){
+				draw:function(urlP){
 					if(!typeof(urlP)=='string'){
 						this.isLoading=false;
 						this.isRunning=false;
 						return;
 					}
-					this.canvas = document.getElementById("renderCanvas"); // Get the canvas element 
 					if(BABYLON.Engine.isSupported()){
-						var canvas=document.getElementById("renderCanvas");
+						var canvas=this.canvas;
 						this.engine=this.engine==null?new BABYLON.Engine(canvas,true):this.engine;
 						BABYLON.SceneLoader.Load("/",urlP,this.engine,dojo.hitch(this,function(scene){
 							this.scene=scene;
@@ -176,7 +124,6 @@ require(
 
 					}
 				},
-
 				update:function(obj,callback){
 					this._contextObj=obj;
 					this._updateRendering(callback);
@@ -188,18 +135,12 @@ require(
 				_updateRendering:function(callback){
 					if(this._contextObj!==null){
 						if(!this.isLoading){
-							//dojoStyle.set(this.domNode,"display","block");
-							console.log('0----------------------------------------');
-							console.log(this._contextObj);
 							var _url='file?guid='+this._contextObj.getGuid()+'&cachebust='+(new Date().getTime())+'&filename='+(this._contextObj.get('Name')==null||this._contextObj.get('Name')==''?'.babylon':this._contextObj.get('Name'));
-							console.log(_url);
-							console.log('1----------------------------------------');
 							try{
 								this.isLoading=true;
-								//this.test1('a.babylon');//'BoomBox.gltf');//'cornellBox.glb');//'Duck.gltf');//'a.babylon');//url);
 								this.engine!=null?this.engine.stopRenderLoop():null;
 								this.scene!=null?this.scene.dispose():null;
-								this.test1(_url);
+								this.draw(_url);
 								this.isRunning=true;
 							}catch(e){
 								console.error(e);
